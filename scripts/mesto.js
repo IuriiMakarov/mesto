8f66c6fa42000ac179619431805af4c3759a420a
Iurii Makarov <makarovyuraluun@gmail.com>
2022-02-10 13:35:25 +0300

    second

--- scripts/mesto.js
+++ scripts/mesto.js
@@ -1,36 +1,43 @@
-var buttonEdit = document.querySelector('.profile__editButton');
-var buttonClose = document.querySelector('.popup__closebtm');
-var overlay = document.querySelector('.popup');
-var overlayActiveClass = 'popup_active';
-var buttonSave = document.querySelector('.popup__savebtm');
+const buttonEdit = document.querySelector('.profile__editButton');
+const buttonClose = document.querySelector('.popup__close-btn');
+const overlay = document.querySelector('.popup');
+const overlayActiveClass = 'popup_active';
+const buttonSave = document.querySelector('.popup__save-btn');
+const profileTitle = document.querySelector('.profile__title');
+const profileSubTitle = document.querySelector('.profile__sub-title');
+const formElement = document.querySelector('.popup');
+const nameInput = document.querySelector('.popup__input_type_name');
+const jobInput = document.querySelector('.popup__input_type_job');
+
+function closeProfilePopup() {
+  overlay.classList.remove(overlayActiveClass)    
+};
 
 buttonEdit.addEventListener('click', function(){
   overlay.classList.add(overlayActiveClass);
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubTitle.textContent;
 });
 
 buttonClose.addEventListener('click', function(){
-  overlay.classList.remove(overlayActiveClass)    
+  closeProfilePopup()
 });
 
 document.addEventListener('keydown', function(event){
   if (event.code === 'Escape') {
-    overlay.classList.remove(overlayActiveClass)
+    closeProfilePopup()
   }
 });
 
 buttonSave.addEventListener('click', function(){
-    overlay.classList.remove(overlayActiveClass);
+  closeProfilePopup()
 })   
 
-let formElement = document.querySelector('.popup');
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__job');
-
-function formSubmitHandler (evt) {
+function handleProfileFormSubmit (evt) {
   evt.preventDefault();
-  document.querySelector('.profile__title').textContent = nameInput.value;
-  document.querySelector('.profile__sub-title').textContent = jobInput.value;
+  profileTitle.textContent = nameInput.value;
+  profileSubTitle.textContent = jobInput.value;
   
 };
 
-formElement.addEventListener('submit', formSubmitHandler); 
+formElement.addEventListener('submit', handleProfileFormSubmit); 
